51ec9a8d1b2fc4ce6f8842303fdd104515b3096b
Josh <joshschulze05@gmail.com>
2023-12-05 15:41:24 -0500

    added code to css,js and index

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var generateBtn = document.querySelector("#generate");
+
+// Get references to the #generate element
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
