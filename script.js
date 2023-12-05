a9e825074b35088749bd4a2702dbe90376c644c7
Josh <joshschulze05@gmail.com>
2023-12-05 15:52:07 -0500

    added promt for amount for password

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 
+var cUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var clower = ["abcdefghijklmnopqrstuvwxyz"];
+var CNumber=[ "0123456789"];
+var CSpecial=["!@,#$%&*{}[]/^)("]
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Get references to the #generate element
+
+var generatePassword= function(){
+
+    var Cchoice=[];
+    var password="";
+    var TotalChoice="";
+  
+  
+    var CLength=parseInt(prompt("Enter legnth of password (between 8-128 charchters)."));
+  
+  
+    if (isNaN(CLength)||CLength<8 || CLength>128) {
+      alert("enter correct amount!");
+      return  false;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Write password to the #password input
